78b6bd96078683fbbf819540c96b071497f21556
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-06-01 22:02:42 +0330

    Update item3d.mjs

--- processor/item3d.mjs
+++ processor/item3d.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 import sharp from "sharp";
 
+const DEPTH = 4;
 const input = process.argv[2];
 const output = process.argv[3];
 
@@ -14,23 +15,33 @@ let faces = [];
 let index = 1;
 
 function addCube(x, y) {
-  const z = 0;
+  const z0 = 0;
+  const z1 = DEPTH;
+
+  const start = index;
 
   const v = [
-    [x, y, z],
-    [x + 1, y, z],
-    [x + 1, y + 1, z],
-    [x, y + 1, z],
-    [x, y, z + 1],
-    [x + 1, y, z + 1],
-    [x + 1, y + 1, z + 1],
-    [x, y + 1, z + 1]
+    [x, y, z0],
+    [x + 1, y, z0],
+    [x + 1, y + 1, z0],
+    [x, y + 1, z0],
+
+    [x, y, z1],
+    [x + 1, y, z1],
+    [x + 1, y + 1, z1],
+    [x, y + 1, z1]
   ];
 
   v.forEach(p => vertices.push(`v ${p[0]} ${p[1]} ${p[2]}`));
 
-  faces.push(`f ${index} ${index+1} ${index+2} ${index+3}`);
-  faces.push(`f ${index+4} ${index+5} ${index+6} ${index+7}`);
+  faces.push(`f ${start} ${start+1} ${start+2} ${start+3}`); // front
+  faces.push(`f ${start+4} ${start+5} ${start+6} ${start+7}`); // back
+
+  faces.push(`f ${start} ${start+4} ${start+7} ${start+3}`); // left
+  faces.push(`f ${start+1} ${start+5} ${start+6} ${start+2}`); // right
+
+  faces.push(`f ${start+3} ${start+2} ${start+6} ${start+7}`); // top
+  faces.push(`f ${start} ${start+1} ${start+5} ${start+4}`); // bottom
 
   index += 8;
 }
