3d6049f8305cd669b99d2bb6371a05903ebe6ced
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-06-02 20:28:25 +0330

    Update item3d.mjs

--- processor/item3d.mjs
+++ processor/item3d.mjs
@@ -7,7 +7,6 @@ const input = process.argv[2];
 const output = process.argv[3];
 
 const DEPTH = 0.8;
-const OVERLAP = 0;
 const ALPHA_THRESHOLD = 40;
 
 const { data, info } = await sharp(input)
@@ -30,96 +29,80 @@ const faces = [];
 let vertexIndex = 1;
 let uvIndex = 1;
 
-function addVertex(x, y, z) {
-  vertices.push(`v ${x} ${y} ${z}`);
-}
-
-function addUV(u, v) {
-  uvs.push(`vt ${u} ${v}`);
-}
-
-function quadToTriangles(v1,v2,v3,v4, t1,t2,t3,t4) {
-
-  faces.push(
-    `f ${v1}/${t1} ${v2}/${t2} ${v3}/${t3}`
-  );
-
-  faces.push(
-    `f ${v1}/${t1} ${v3}/${t3} ${v4}/${t4}`
-  );
+function addQuad(a, b, c, d, ta, tb, tc, td) {
+  faces.push(`f ${a}/${ta} ${b}/${tb} ${c}/${tc}`);
+  faces.push(`f ${a}/${ta} ${c}/${tc} ${d}/${td}`);
 }
 
 for (let y = 0; y < info.height; y++) {
-
   for (let x = 0; x < info.width; x++) {
 
-    if (!isSolid(x, y))
-      continue;
+    if (!isSolid(x, y)) continue;
 
-const px = x;
-const py = info.height - y - 1;
+    const px = x;
+    const py = info.height - y - 1;
 
-const pz0 = -DEPTH / 2;
-const pz1 = DEPTH / 2;
+    const z0 = -DEPTH / 2;
+    const z1 = DEPTH / 2;
 
-vertices.push(`v ${px} ${py} ${pz0}`);
-vertices.push(`v ${px + 1} ${py} ${pz0}`);
-vertices.push(`v ${px + 1} ${py + 1} ${pz0}`);
-vertices.push(`v ${px} ${py + 1} ${pz0}`);
+    const vb = vertexIndex;
+    const tb = uvIndex;
 
-vertices.push(`v ${px} ${py} ${pz1}`);
-vertices.push(`v ${px + 1} ${py} ${pz1}`);
-vertices.push(`v ${px + 1} ${py + 1} ${pz1}`);
-vertices.push(`v ${px} ${py + 1} ${pz1}`);
+    vertices.push(`v ${px} ${py} ${z0}`);
+    vertices.push(`v ${px + 1} ${py} ${z0}`);
+    vertices.push(`v ${px + 1} ${py + 1} ${z0}`);
+    vertices.push(`v ${px} ${py + 1} ${z0}`);
+
+    vertices.push(`v ${px} ${py} ${z1}`);
+    vertices.push(`v ${px + 1} ${py} ${z1}`);
+    vertices.push(`v ${px + 1} ${py + 1} ${z1}`);
+    vertices.push(`v ${px} ${py + 1} ${z1}`);
 
     const u1 = x / info.width;
     const u2 = (x + 1) / info.width;
-
     const v1 = (info.height - y - 1) / info.height;
     const v2 = (info.height - y) / info.height;
 
-    addUV(u1, v1);
-    addUV(u2, v1);
-    addUV(u2, v2);
-    addUV(u1, v2);
+    uvs.push(`vt ${u1} ${v1}`);
+    uvs.push(`vt ${u2} ${v1}`);
+    uvs.push(`vt ${u2} ${v2}`);
+    uvs.push(`vt ${u1} ${v2}`);
 
-    quadToTriangles(
+    // FRONT
+    addQuad(
       vb+4, vb+5, vb+6, vb+7,
       tb, tb+1, tb+2, tb+3
     );
 
-    quadToTriangles(
-      vb+0, vb+3, vb+2, vb+1,
-      tb, tb+3, tb+2, tb+1
+    // BACK
+    addQuad(
+      vb+1, vb+0, vb+3, vb+2,
+      tb+1, tb, tb+3, tb+2
     );
 
-    if (!isSolid(x - 1, y)) {
-      quadToTriangles(
-        vb+0, vb+4, vb+7, vb+3,
-        tb, tb, tb+3, tb+3
-      );
-    }
+    // LEFT
+    addQuad(
+      vb+0, vb+4, vb+7, vb+3,
+      tb, tb, tb+3, tb+3
+    );
 
-    if (!isSolid(x + 1, y)) {
-      quadToTriangles(
-        vb+1, vb+2, vb+6, vb+5,
-        tb+1, tb+2, tb+2, tb+1
-      );
-    }
+    // RIGHT
+    addQuad(
+      vb+5, vb+1, vb+2, vb+6,
+      tb+1, tb+1, tb+2, tb+2
+    );
 
-    if (!isSolid(x, y - 1)) {
-      quadToTriangles(
-        vb+3, vb+7, vb+6, vb+2,
-        tb+3, tb+3, tb+2, tb+2
-      );
-    }
+    // TOP
+    addQuad(
+      vb+3, vb+7, vb+6, vb+2,
+      tb+3, tb+3, tb+2, tb+2
+    );
 
-    if (!isSolid(x, y + 1)) {
-      quadToTriangles(
-        vb+0, vb+1, vb+5, vb+4,
-        tb, tb+1, tb+1, tb
-      );
-    }
+    // BOTTOM
+    addQuad(
+      vb+0, vb+1, vb+5, vb+4,
+      tb, tb+1, tb+1, tb
+    );
 
     vertexIndex += 8;
     uvIndex += 4;
@@ -153,10 +136,7 @@ map_Kd ${baseName}.png
 `
 );
 
-fs.copyFileSync(
-  input,
-  output.replace(".obj", ".png")
-);
+fs.copyFileSync(input, output.replace(".obj", ".png"));
 
 const zipPath = output.replace(".obj", ".zip");
 
@@ -168,22 +148,10 @@ const stream = fs.createWriteStream(zipPath);
 
 archive.pipe(stream);
 
-archive.file(output, {
-  name: `${baseName}.obj`
-});
-
-archive.file(
-  output.replace(".obj", ".mtl"),
-  { name: `${baseName}.mtl` }
-);
-
-archive.file(
-  output.replace(".obj", ".png"),
-  { name: `${baseName}.png` }
-);
+archive.file(output, { name: `${baseName}.obj` });
+archive.file(output.replace(".obj", ".mtl"), { name: `${baseName}.mtl` });
+archive.file(output.replace(".obj", ".png"), { name: `${baseName}.png` });
 
 await archive.finalize();
 
-console.log(
-  `✅ Exported (${info.width}x${info.height})`
-);
+console.log(`✅ Exported (${info.width}x${info.height})`);
