8b6b97051d2d4e1de5863fb7efe75e2ccd1111ee
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-06-02 10:03:34 +0330

    Update item3d.mjs

--- processor/item3d.mjs
+++ processor/item3d.mjs
@@ -16,36 +16,18 @@ let vertices = [];
 let faces = [];
 let index = 1;
 
-function addCube(x, y) {
-  const z0 = 0;
-  const z1 = DEPTH;
+function isSolid(x, y) {
+  if (
+    x < 0 ||
+    y < 0 ||
+    x >= info.width ||
+    y >= info.height
+  ) {
+    return false;
+  }
 
-  const start = index;
-
-  const v = [
-    [x, y, z0],
-    [x + 1, y, z0],
-    [x + 1, y + 1, z0],
-    [x, y + 1, z0],
-
-    [x, y, z1],
-    [x + 1, y, z1],
-    [x + 1, y + 1, z1],
-    [x, y + 1, z1]
-  ];
-
-  v.forEach(p => vertices.push(`v ${p[0]} ${p[1]} ${p[2]}`));
-
-  faces.push(`f ${start} ${start+1} ${start+2} ${start+3}`); // front
-  faces.push(`f ${start+4} ${start+5} ${start+6} ${start+7}`); // back
-
-  faces.push(`f ${start} ${start+4} ${start+7} ${start+3}`); // left
-  faces.push(`f ${start+1} ${start+5} ${start+6} ${start+2}`); // right
-
-  faces.push(`f ${start+3} ${start+2} ${start+6} ${start+7}`); // top
-  faces.push(`f ${start} ${start+1} ${start+5} ${start+4}`); // bottom
-
-  index += 8;
+  const i = (y * info.width + x) * 4;
+  return data[i + 3] > 0;
 }
 
 for (let y = 0; y < info.height; y++) {
