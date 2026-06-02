8c16a263134c7a4904709e3814a172f3347633fb
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-06-02 19:23:17 +0330

    Update item3d.mjs

--- processor/item3d.mjs
+++ processor/item3d.mjs
@@ -7,6 +7,8 @@ const input = process.argv[2];
 const output = process.argv[3];
 
 const DEPTH = 0.8; // ضخامت
+
+// خواندن تصویر
 const { data, info } = await sharp(input)
   .ensureAlpha()
   .raw()
@@ -25,7 +27,7 @@ function isSolid(x, y) {
 }
 
 function addFace(v1, v2, v3, v4, uv1, uv2, uv3, uv4) {
-  faces.push(`f ${v1}/${uv1} ${v2}/${uv2} ${v3}/${uv3} ${v4}/${uv4}`);
+  faces.push(`f ${v1}/${uv1} ${v2}/${uv2} ${v3}/${uv3} ${uv4}/${uv4}`);
 }
 
 for (let y = 0; y < info.height; y++) {
@@ -38,19 +40,21 @@ for (let y = 0; y < info.height; y++) {
     const pz1 = DEPTH / 2;
 
     const v0 = vIndex;
-    vertices.push(`v ${px} ${py} ${pz0}`); // 0
-    vertices.push(`v ${px+1} ${py} ${pz0}`); // 1
-    vertices.push(`v ${px+1} ${py+1} ${pz0}`); // 2
-    vertices.push(`v ${px} ${py+1} ${pz0}`); // 3
 
-    vertices.push(`v ${px} ${py} ${pz1}`); // 4
-    vertices.push(`v ${px+1} ${py} ${pz1}`); // 5
-    vertices.push(`v ${px+1} ${py+1} ${pz1}`); // 6
-    vertices.push(`v ${px} ${py+1} ${pz1}`); // 7
+    // 8 vertex
+    vertices.push(`v ${px} ${py} ${pz0}`);
+    vertices.push(`v ${px+1} ${py} ${pz0}`);
+    vertices.push(`v ${px+1} ${py+1} ${pz0}`);
+    vertices.push(`v ${px} ${py+1} ${pz0}`);
+
+    vertices.push(`v ${px} ${py} ${pz1}`);
+    vertices.push(`v ${px+1} ${py} ${pz1}`);
+    vertices.push(`v ${px+1} ${py+1} ${pz1}`);
+    vertices.push(`v ${px} ${py+1} ${pz1}`);
 
     vIndex += 8;
 
-    // UVs مخصوص هر پیکسل
+    // UV مخصوص top/bottom
     const u1 = x / info.width;
     const u2 = (x + 1) / info.width;
     const v1 = (info.height - y - 1) / info.height;
@@ -64,63 +68,39 @@ for (let y = 0; y < info.height; y++) {
     uvIndex += 4;
 
     // top
-    addFace(v0+4, v0+5, v0+6, v0+7, uv0, uv0+1, uv0+2, uv0+3);
+    faces.push(`f ${v0+4}/${uv0} ${v0+5}/${uv0+1} ${v0+6}/${uv0+2} ${v0+7}/${uv0+3}`);
 
     // bottom
-    addFace(v0+0, v0+1, v0+2, v0+3, uv0, uv0+1, uv0+2, uv0+3);
+    faces.push(`f ${v0+0}/${uv0} ${v0+1}/${uv0+1} ${v0+2}/${uv0+2} ${v0+3}/${uv0+3}`);
+
+    // side faces (UV مستقل)
+    function side(uvi, a,b,c,d) {
+      const u = uvi;
+      uvs.push(`vt 0 0`);
+      uvs.push(`vt 1 0`);
+      uvs.push(`vt 1 1`);
+      uvs.push(`vt 0 1`);
+      faces.push(`f ${a}/${u} ${b}/${u+1} ${c}/${u+2} ${d}/${u+3}`);
+      return u + 4;
+    }
 
     // left
-    if (!isSolid(x-1, y)) {
-      const u = uvIndex;
-      uvs.push(`vt 0 0`);
-      uvs.push(`vt 1 0`);
-      uvs.push(`vt 1 1`);
-      uvs.push(`vt 0 1`);
-      uvIndex += 4;
-
-      addFace(v0+0, v0+4, v0+7, v0+3, u, u+1, u+2, u+3);
-    }
+    if (!isSolid(x-1, y)) uvIndex = side(uvIndex, v0+0, v0+4, v0+7, v0+3);
 
     // right
-    if (!isSolid(x+1, y)) {
-      const u = uvIndex;
-      uvs.push(`vt 0 0`);
-      uvs.push(`vt 1 0`);
-      uvs.push(`vt 1 1`);
-      uvs.push(`vt 0 1`);
-      uvIndex += 4;
-
-      addFace(v0+1, v0+2, v0+6, v0+5, u, u+1, u+2, u+3);
-    }
+    if (!isSolid(x+1, y)) uvIndex = side(uvIndex, v0+1, v0+2, v0+6, v0+5);
 
     // down
-    if (!isSolid(x, y-1)) {
-      const u = uvIndex;
-      uvs.push(`vt 0 0`);
-      uvs.push(`vt 1 0`);
-      uvs.push(`vt 1 1`);
-      uvs.push(`vt 0 1`);
-      uvIndex += 4;
-
-      addFace(v0+3, v0+2, v0+6, v0+7, u, u+1, u+2, u+3);
-    }
+    if (!isSolid(x, y-1)) uvIndex = side(uvIndex, v0+3, v0+2, v0+6, v0+7);
 
     // up
-    if (!isSolid(x, y+1)) {
-      const u = uvIndex;
-      uvs.push(`vt 0 0`);
-      uvs.push(`vt 1 0`);
-      uvs.push(`vt 1 1`);
-      uvs.push(`vt 0 1`);
-      uvIndex += 4;
-
-      addFace(v0+0, v0+1, v0+5, v0+4, u, u+1, u+2, u+3);
-    }
+    if (!isSolid(x, y+1)) uvIndex = side(uvIndex, v0+0, v0+1, v0+5, v0+4);
   }
 }
 
 const baseName = path.basename(output, ".obj");
 
+// OBJ
 const objContent = `mtllib ${baseName}.mtl
 usemtl Material
 
@@ -130,6 +110,7 @@ ${faces.join("\n")}`;
 
 fs.writeFileSync(output, objContent);
 
+// MTL
 fs.writeFileSync(output.replace(".obj", ".mtl"), 
 `newmtl Material
 Ka 1 1 1
@@ -140,8 +121,10 @@ illum 2
 map_Kd ${baseName}.png
 `);
 
+// PNG
 fs.copyFileSync(input, output.replace(".obj", ".png"));
 
+// ZIP
 const zipPath = output.replace(".obj", ".zip");
 const archive = archiver("zip", { zlib: { level: 9 } });
 const stream = fs.createWriteStream(zipPath);
@@ -149,7 +132,7 @@ archive.pipe(stream);
 
 archive.file(output, { name: `${baseName}.obj` });
 archive.file(output.replace(".obj", ".mtl"), { name: `${baseName}.mtl` });
-archive.file(output.replace(".obj", ".png`), { name: `${baseName}.png` });
+archive.file(output.replace(".obj", ".png"), { name: `${baseName}.png` });
 
 await archive.finalize();
 
