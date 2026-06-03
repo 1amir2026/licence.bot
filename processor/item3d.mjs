bf4e83b56dff65d0984663197b19f7f3440a3c72
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-06-03 11:47:49 +0330

    Update item3d.mjs

--- processor/item3d.mjs
+++ processor/item3d.mjs
@@ -12,7 +12,7 @@ if (!input || !output) {
 }
 
 const SCALE = 1 / 16;
-const DEPTH = Number(process.argv[4] || 0.8);
+const DEPTH = 0.8;
 const ALPHA_THRESHOLD = 40;
 
 const { data, info } = await sharp(input)
