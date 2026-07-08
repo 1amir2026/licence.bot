4dab725c33cb8868f6ccecb9f328022532e7c39a
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-07-08 07:34:47 -0700

    Update types.js

---
+++ processor/types.js
@@ -0,0 +1,88 @@
+export type ICoordinatesType = "ICON" | "GUI";
+
+type _ICoordinates = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
+type IIconCoordinates = {
+    xpBg: _ICoordinates;
+    heart: _ICoordinates;
+    hunger: _ICoordinates;
+    heartBg: _ICoordinates;
+    hungerBg: _ICoordinates;
+    xp: _ICoordinates;
+    armor: _ICoordinates;
+};
+
+type IGUICoordinates = {
+    twoSlots: _ICoordinates;
+    lastSlot: _ICoordinates;
+    selector: _ICoordinates;
+};
+
+export type ICoordinates<T extends ICoordinatesType> = T extends "ICON"
+    ? IIconCoordinates
+    : T extends "GUI"
+    ? IGUICoordinates
+    : never;
+
+type IIconPaths = {
+    xpBg: string;
+    heart: string;
+    hunger: string;
+    hungerBg: string;
+    heartBg: string;
+    xp: string;
+    armor: string;
+};
+
+type IGUIPaths = {
+    twoSlots: string;
+    lastSlot: string;
+    selector: string;
+};
+
+type ISysPaths = {
+    tempPath: string;
+    packFolder: string;
+    packGuiFolder: string;
+    packIconsPath: string;
+    packWidgetsPath: string;
+    tempIconsPath: string;
+    tempWidgetsPath: string;
+    configPath: string;
+};
+
+export type IPathType = "SYS" | "ICON" | "GUI";
+
+export type IPaths<T extends IPathType> = T extends "ICON"
+    ? IIconPaths
+    : T extends "GUI"
+    ? IGUIPaths
+    : T extends "SYS"
+    ? ISysPaths
+    : never;
+
+export type IConfig = {
+    packFileName: string;
+    scalingFactor: number;
+    bedrock: boolean;
+    xpPercent: number;
+    upscaleRate: number;
+};
+
+export type IIconInfo = {
+    name: string;
+    path: string | Buffer;
+    destCoordinates: _ICoordinates;
+};
+
+export type IPackData = {
+    packName: string;
+    packBufferString: string;
+    xpPercent: number;
+    upscaleRate: number;
+};
